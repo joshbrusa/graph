592fe7a9f76426f9ae53c95f83c7196464780ce8
joshbrusa <joshbrusa@gmail.com>
2024-03-01 22:56:05 -0800

    save

--- src/Graph.java
+++ src/Graph.java
@@ -1,15 +1,39 @@
 package src;
 
-import java.util.LinkedList;
-
+/**
+ * Our graph date structure.
+ */
 class Graph {
-    private int v;
-    private LinkedList<Integer>[] adj;
+    private int vertexCount;
+    private IntegerLinkedList[] adjacencyList;
 
-    public Graph(int v) {
-        this.v = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; ++i)
-            adj[i] = new LinkedList();
+    public Graph(int vertexCount) {
+        this.vertexCount = vertexCount;
+        this.adjacencyList = new IntegerLinkedList[this.vertexCount];
+
+        for (int i = 0; i < this.vertexCount; ++i) {
+            this.adjacencyList[i] = new IntegerLinkedList();
+        }
+    }
+
+    /**
+     * Adds an edge given a starting vertex and an ending vertex.
+     */
+    public void addEdge(int startVertex, int endVertex) {
+        this.adjacencyList[startVertex].add(endVertex);
+    }
+
+    /**
+     * Depth first search.
+     */
+    public void dfs() {
+
+    }
+
+    /**
+     * Breadth first search.
+     */
+    public void bfs() {
+
     }
 }
---
+++ src/IntegerLinkedList.java
@@ -0,0 +1,9 @@
+package src;
+
+import java.util.LinkedList;
+
+/**
+ * This class prevents a linter warning.
+ */
+class IntegerLinkedList extends LinkedList<Integer> {
+}
